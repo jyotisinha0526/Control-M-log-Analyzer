83cd80e07f1ab90c7bd2cb1ae7cc2d05277d1f98
Jyoti Sinha <jyotisinha0526@gmail.com>
2026-04-07 00:39:31 +0800

    Updated BMC URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -120,7 +120,7 @@ function App() {
     <h3 style={{ color: "#4FC3F7" }}>📂 AI Response</h3>
 	{/* 🔍 BMC Search Link */}
 <a
-  href={`https://webapps.bmc.com/helixsupport/assets/search.html#q=${encodeURIComponent(query)}`}
+  //href={#}
   target="_blank"
   rel="noreferrer"
   style={{
